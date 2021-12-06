94ab9b655acbecec07012c45f8ac5abbb7b7ebf0
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-12-06 14:54:06 -0500

    front

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { useEffect } from "react";
-<<<<<<< HEAD
 import NavBar from "./NavBar";
 import "./App.css";
 
-=======
-// testing upload
->>>>>>> 9c024eaf36fc91ad654f62b296508ae134720a8e
 function App() {
   useEffect(() => {
     fetch("http://localhost:9292/")
