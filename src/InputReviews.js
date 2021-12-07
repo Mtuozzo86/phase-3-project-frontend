7774d47113a12d3100a850e8c9c7d4e651a86293
Mtuozzo86 <mtuozzo86@gmail.com>
2021-12-07 18:18:11 -0500

    adding state

--- src/InputReviews.js
+++ src/InputReviews.js
@@ -1,6 +1,19 @@
 import "./InputReviews.css";
+import { useState } from "react";
 
 function InputReviews() {
+  const [userName, setUserName] = useState();
+  const [body, setBody] = useState("");
+
+  function handleNameInput(e) {
+    setUserName(e.target.value);
+    console.log(userName);
+  }
+
+  function handleReviewBody(e) {
+    console.log(e.target.value);
+  }
+
   return (
     <div>
       <form>
@@ -11,6 +24,7 @@ function InputReviews() {
             id="name"
             name="name"
             placeholder="Your name..."
+            onChange={handleNameInput}
           ></input>
         </div>
         <div>
@@ -20,6 +34,7 @@ function InputReviews() {
             cols="30"
             rows="4"
             placeholder="Leave a review..."
+            onChange={handleReviewBody}
           ></textarea>
         </div>
         <div>
